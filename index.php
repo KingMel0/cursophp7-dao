aee154c94140c5e890cd0cefe1ed173a0844850a
Marco Tulio <marcofc6@gmail.com>
2022-07-06 12:37:46 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -8,9 +8,17 @@ var_dump(require_once("config.php"));
 //echo $root;
 
 //carrega todos os usuarios
-var_dump($lista = Usuario::getList());
+//var_dump($lista = Usuario::getList());
+//echo json_encode($lista)
 
-echo json_encode($lista)
+//carrega uma lita de usuários buscado pelo login
+//$search = Usuario::search("m");
+//echo json_encode($search);
+
+//carrega um usuário usando o login e senha
+$usuario = new Usuario();
+$usuario->login("tulio","idk");
+echo $usuario;
 
 
 ?>
